3054fd21d225de4da4b2d67c2fd3f0786267a6be
David Jensen <david.aka.dj@gmail.com>
2012-11-18 08:17:45 -0700

    deleted some test files

--- DBFlyer.js
+++
@@ -1,6 +0,0 @@
-<NotepadPlus>
-    <Session activeView="0">
-        <mainView activeIndex="3" />
-        <subView activeIndex="0" />
-    </Session>
-</NotepadPlus>
--- test.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-
-  var str = "[1,2,3,3,3,3,3,3]";
-
-  var str_eval = eval(str);
-
-  alert(str_eval[1]);
-  
-});
